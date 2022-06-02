127469379ebd2ef4b70dcbde1c6951b46db8ca7a
MrKaaLxD <105182342+mrkaalxd@users.noreply.github.com>
2022-06-02 08:20:21 +0530

    Update __main__.py

--- modules/__main__.py
+++ modules/__main__.py
@@ -1,16 +1,17 @@
 from pyrogram import idle
 from pyrogram import Client as Bot
+from modules import bot
 from modules.clientbot import run
 from modules.config import API_ID, API_HASH, BOT_TOKEN
 
     
-bot = Bot(
-    ":memory:",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="plugins")
-)
+#bot = Bot(
+#    ":memory:",
+#    API_ID,
+#    API_HASH,
+#    bot_token=BOT_TOKEN,
+#    plugins=dict(root="plugins")
+#)
 
 bot.start()
 run()
