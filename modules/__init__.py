08ecb177d3d4cafda3f79cdc47ce97a794779837
MrKaaLxD <105182342+mrkaalxd@users.noreply.github.com>
2022-06-02 10:04:26 +0530

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,11 +1,16 @@
 from pyrogram import Client as Bot
 from modules.config import API_ID, API_HASH, BOT_TOKEN
-
+from modules.clientbot.clientbot import client
     
 bot = Bot(
-    ":memory:",
+    ":kaal:",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
     plugins=dict(root="plugins")
 )
+
+with Bot(":kaal:", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
+    me_bot = app.get_me()
+with user as app:
+    me_user = app.get_me()
