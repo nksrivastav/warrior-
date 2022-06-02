9fa93595d4690e4cf31e3bdda6e229fe0352648d
MrKaaLxD <105182342+mrkaalxd@users.noreply.github.com>
2022-06-02 17:53:11 +0530

    Update __main__.py

--- modules/__main__.py
+++ modules/__main__.py
@@ -4,14 +4,7 @@ from modules import bot
 from modules.clientbot import run
 from modules.config import API_ID, API_HASH, BOT_TOKEN
 
-    
-#bot = Bot(
-#    ":memory:",
-#    API_ID,
-#    API_HASH,
-#    bot_token=BOT_TOKEN,
-#    plugins=dict(root="plugins")
-#)
+
 
 bot.start()
 run()
