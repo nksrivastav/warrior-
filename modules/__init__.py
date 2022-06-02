4cff83a78acbf9ef686ab34ff37a830d2c90f188
MrKaaLxD <105182342+mrkaalxd@users.noreply.github.com>
2022-06-02 17:52:50 +0530

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -9,8 +9,3 @@ bot = Bot(
     bot_token=BOT_TOKEN,
     plugins=dict(root="plugins")
 )
-
-with Bot(":kaal:", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
-    me_bot = app.get_me()
-with client as app:
-    me_user = app.get_me()
