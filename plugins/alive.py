12996b6fd6a3e4807451cc773fde6cad895cc050
toxictelegram1 <94620297+toxictelegram1@users.noreply.github.com>
2022-06-28 11:37:58 +0545

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -57,7 +57,7 @@ async def start_(client: Client, message: Message):
     )
     
     
-@Client.on_message(commandpro(["/start", "/alive", "#aditya"]) & filters.group & ~filters.edited)
+@Client.on_message(commandpro(["/start", "/alive", "#Toxic"]) & filters.group & ~filters.edited)
 async def start(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://te.legra.ph/file/c6e1041c6c9a12913f57a.png",
